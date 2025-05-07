c60187b7fc0fea99253f14c69144091b6b726f77
Ole <ortmann.ole@gmail.com>
2025-05-08 00:23:19 +0200

    CreatedSlideClass

---
+++ UE_Grapple2/Source/UE_Grapple2/Private/Player/Slide.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/Slide.h"
+
+
+// Sets default values for this component's properties
+USlide::USlide()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void USlide::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void USlide::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void USlide::StartSlide_Implementation()
+{
+	
+}
+
+void USlide::EndSlide_Implementation()
+{
+	
+}
+
---
+++ UE_Grapple2/Source/UE_Grapple2/Public/Player/Slide.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Slide.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class UE_GRAPPLE2_API USlide : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	USlide();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void StartSlide();
+	void StartSlide_Implementation();
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void EndSlide();
+	void EndSlide_Implementation();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsSliding=false;
+};
