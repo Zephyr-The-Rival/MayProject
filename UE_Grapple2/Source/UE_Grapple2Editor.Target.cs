32d9b4d969388956aa4cbe3bbe47a581763df3a5
Ole <ortmann.ole@gmail.com>
2025-12-10 22:36:36 +0100

    Engine Version Change

--- UE_Grapple2/Source/UE_Grapple2Editor.Target.cs
+++ UE_Grapple2/Source/UE_Grapple2Editor.Target.cs
@@ -8,8 +8,8 @@ public class UE_Grapple2EditorTarget : TargetRules
 	public UE_Grapple2EditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("UE_Grapple2");
 	}
 }
